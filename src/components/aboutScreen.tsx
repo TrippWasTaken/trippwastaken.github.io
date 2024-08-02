1d45905bed318def0ca66bad399997c6ae05aa08
TrippWasTaken <konrad.knecht@gmail.com>
2024-08-02 18:18:33 +0100

    start about screen

--- src/components/aboutScreen.tsx
+++ src/components/aboutScreen.tsx
@@ -4,7 +4,22 @@ const AboutScreen = () => {
   return (
     <PageContainer>
       <div className="bg-zinc-200 absolute w-full h-full z-0" />
-      <div className="bg-zinc-200 text-zinc-900">About me</div>
+      <div className="text-zinc-900 z-10 self-start justify-self-start place-self-start w-full pt-20">
+        <h3 className="text-[6rem] grid grid-flow-col">
+          <div className="relative top-0">
+            <span>WHO?</span>
+            <span className="opacity-15 absolute text-nowrap top-10 left-10 font-extrabold">誰か</span>
+          </div>
+          <div className="relative top-0">
+            <span>WHAT?</span>
+            <span className="opacity-15 absolute text-nowrap top-10 left-10 font-extrabold">何か</span>
+          </div>
+          <div className="relative top-0">
+            <span>WHERE?</span>
+            <span className="opacity-15 absolute text-nowrap top-10 left-10 font-extrabold">どこに</span>
+          </div>
+        </h3>
+      </div>
     </PageContainer>
   );
 };
